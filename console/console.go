44f47cd5ca664e7450bb50513be8ca532c58f43b
李双全 <294388344@qq.com>
2023-02-06 09:53:34 +0800

    add console

--- console/console.go
+++ console/console.go
@@ -1,1 +1,15 @@
 package console
+
+import "fmt"
+
+func WriteLine(a ...any) {
+	fmt.Println(a)
+}
+func Write(a ...any) {
+	fmt.Print(a)
+}
+func ReadLine() string {
+	var str string
+	fmt.Scan(&str)
+	return str
+}
